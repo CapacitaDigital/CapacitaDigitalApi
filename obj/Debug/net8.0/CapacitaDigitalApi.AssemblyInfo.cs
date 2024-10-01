dcd0eb8a7dca2477ec84033e38a79468b86376a4
jjamerson-mt <jjamersonmt@gmail.com>
2024-10-01 16:37:30 -0300

    ok

--- obj/Debug/net8.0/CapacitaDigitalApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CapacitaDigitalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapacitaDigitalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda324349a7e83041cb3eb05f3371e94cba9f632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b939caabcd8608d12116406dddd0e959dcabd6f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapacitaDigitalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapacitaDigitalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
