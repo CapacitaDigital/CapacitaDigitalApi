b9b937dbbfcf2a642d1d2f360e495c64034a6fd8
jjamerson-mt <jjamersonmt@gmail.com>
2024-10-26 00:13:03 -0300

    alterações nos Seeds ()Content nos campos urlVideo e UrlImages e urlDocuments e activityData) e acrescentei algumas imagems e documentos

--- Data/Seeds/ContentSeed.cs
+++ Data/Seeds/ContentSeed.cs
@@ -17,8 +17,8 @@ public static class ContentSeed
                 Title = "Recursos",
                 Description = "Aqui estão todos os recursos deste módulo disponibilizados pelo professor da disciplina",
                 Type = ContentType.Assets,
-                UrlImage = "/images/assets.jpeg",
-                UrlVideo = "https://youtu.be/SEXGH4mG4is?si=E30RYMfxpUODFDYa",
+                UrlImage = "/images/alfabeto.jpeg",
+                UrlVideo = "https://www.youtube.com/embed/MY9KP4m35Tc?si=Gk0qqeXdtvZkjTQD",
                 UrlsDocuments = new List<string>
                 {
                     "/documents/vogais.pdf",
@@ -26,7 +26,7 @@ public static class ContentSeed
                     "/documents/alfabeto.pdf",
                     "/documents/alfabeto.json",
                 },
-                ActivityData = "{\"alfabeto\": [\"A\", \"B\", \"C\", \"D\", \"E\"],\"urlSounds\": [\"/sounds/\"]}",
+                ActivityData = "{\"alfabeto\": [\"A\", \"B\", \"C\", \"D\", \"E\", \"F\", \"G\", \"H\", \"I\", \"J\", \"K\", \"L\", \"M\", \"N\", \"O\", \"P\", \"Q\", \"R\", \"S\", \"T\", \"U\", \"V\", \"W\", \"X\", \"Y\", \"Z\"],\"urlSounds\": [\"/sounds/\"]}",
                 ModuleId = existingModule.Id
             },
             new Content
@@ -35,7 +35,7 @@ public static class ContentSeed
                 Description = "Aprenda as vogais do alfabeto",
                 Type = ContentType.ClassRoom,
                 UrlImage = "/images/vogais.jpeg",
-                UrlVideo = "https://youtu.be/SEXGH4mG4is?si=E30RYMfxpUODFDYa",
+                UrlVideo = "https://www.youtube.com/embed/MY9KP4m35Tc?si=Gk0qqeXdtvZkjTQD",
                 UrlsDocuments = new List<string>
                 {
                     "/documents/vogais.pdf",
@@ -49,14 +49,14 @@ public static class ContentSeed
                 Title = "Consoantes",
                 Description = "Aprenda as consoantes do alfabeto",
                 Type = ContentType.ClassRoom,
-                UrlImage = "/images/consoantes.jpeg",
-                UrlVideo = "https://youtu.be/SEXGH4mG4is?si=E30RYMfxpUODFDYa",
+                UrlImage = "/images/alfabeto.jpeg",
+                UrlVideo = "https://www.youtube.com/embed/MY9KP4m35Tc?si=Gk0qqeXdtvZkjTQD",
                 UrlsDocuments = new List<string>
                 {
                     "/documents/consoantes.pdf",
                     "/documents/consoantes.json"
                 },
-                ActivityData = "{\"consoantes\": [\"B\", \"C\", \"D\", \"F\", \"G\"],\"urlSounds\": [\"/sounds/\"]}",
+             ActivityData = "{\"consoantes\": [\"B\", \"C\", \"D\", \"F\", \"G\", \"H\",  \"J\", \"K\", \"L\", \"M\", \"N\", \"P\", \"Q\", \"R\", \"S\", \"T\", \"V\", \"W\", \"X\", \"Y\", \"Z\"],\"urlSounds\": [\"/sounds/\"]}",
                 ModuleId = existingModule.Id
             }
         );
